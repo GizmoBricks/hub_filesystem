f5daa7e0455c9413a72d1b09066df124814f6813
GizmoBricks <127412675+GizmoBricks@users.noreply.github.com>
2023-12-20 21:17:21 +0100

    Create file_system.py

---
+++ file_system.py
@@ -0,0 +1,26 @@
+import os
+
+
+def walk(start_path: str = '/', level=0):
+
+    items = os.listdir(start_path)
+
+    for item in items:
+        
+        if level == 0:
+            print(item)
+        else:
+            print('|    ' * (level-1) + '|-   ' + item)
+
+        if start_path =='/':
+            path = start_path + item
+        else:
+            path = start_path + '/' + item
+
+        try:  
+            walk(path, level+1)
+        except OSError:
+            pass
+
+
+walk()
